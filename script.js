60cd445db8ab56ddfe2241537742efa648f3315a
Viktoria Stolz <88290448+viktoriastolz@users.noreply.github.com>
2024-12-14 20:49:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 "use strict";
 
+const currencyValue = 26; 
+
+for (let dollars = 10; dollars <= 100; dollars += 10) {
+    const hryvnias = dollars * currencyValue;
+    console.log(`${dollars} dollars = ${hryvnias} hryvnias`);
+}
